8c8fdf4cce02dbf902a79b1d4ce3e5ee87ce84c5
EshaanSandhu <110472900+EshaanSandhu@users.noreply.github.com>
2023-06-13 19:29:38 +0530

    Add files via upload

---
+++ Primary.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class Primary{
+public static void main(String args[]){
+Scanner scanner = new Scanner(System.in);
+System.out.println("Enter First Value");
+int a = scanner.nextInt();
+System.out.println("Enter Second Value");
+int b = scanner.nextInt();
+System.out.println("Enter Third Value");
+int c = scanner.nextInt();
+if(a<=b)
+{
+if(a<=c)
+System.out.println("C is Smallest"+ a);
+else
+System.out.println("A is Smallest"+ c);
+}
+else
+{
+if(b<=c)
+System.out.println("B is Smallest"+ b);
+else
+System.out.println("A is Smallest"+ c);
+}
+}
+}
+
+
+
+ 
